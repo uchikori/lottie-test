349b60bffdeeb259e34d817a12966aa5f2ecb6bd
akiyama-projects <uchiwa.cs@gmail.com>
2024-07-12 15:01:21 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 //CSSの読み込み
 import "./scss/style.scss";
 import { LottiePlayer } from "@lottiefiles/lottie-player";
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateVieportInfo();
+});
+window.addEventListener("resize", updateVieportInfo);
+function updateVieportInfo() {
+  const logicalWidth = window.innerWidth;
+  document.getElementById("viewport-width").textContent =
+    "Viewport Width: " + logicalWidth + "px";
+}
